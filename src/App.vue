c94d1810717559f3df8c2643d4af0b7842b9995f
ManuelTello <93638568+ManuelTello@users.noreply.github.com>
2023-01-20 15:34:22 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "App",
+  props: [],
+  data() {
+    return {
+
+    }
+  },
+  components: {}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "UsersList",
+    created() {
+
+    },
+    data() {
+        return {
+            users: null
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
